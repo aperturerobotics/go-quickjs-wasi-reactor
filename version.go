09c6215f4722af89e80ebba589bb2403ac606933
Christian Stewart <christian@aperture.us>
2025-12-29 00:45:37 -0800

    Update to wasi-sdk-29 optimized build (v0.11.0-wasi29)

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package quickjswasi
 // QuickJS-NG version information
 const (
 	// Version is the QuickJS-NG version
-	Version = "v0.11.0"
+	Version = "v0.11.0-wasi29"
 	// DownloadURL is the URL where this WASM file was downloaded from
-	DownloadURL = "https://github.com/quickjs-ng/quickjs/releases/download/v0.11.0/qjs-wasi.wasm"
+	DownloadURL = "https://github.com/paralin/quickjs/releases/download/v0.11.0-wasi29/qjs.wasm"
 )
